7c8425a0d5b55b9bc32ad3c3200cb9300595e6c5
QuyenTran1002 <btran1002@gatech.edu>
2019-01-23 19:04:09 -0500

    adding weather widget

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {Constants} from 'expo';
 import { Button } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import * as firebase from 'firebase';
+import { WeatherWidget } from 'react-native-weather';
 
 const config = {
   apiKey: "AIzaSyBasnhoBBPBGCUDT81DTyXXrZkQ-aR1ZaY",
@@ -21,41 +22,78 @@ export default class App extends React.Component {
     this.state = {
       username: '',
       password: '',
+      info: '',
+      major: '',
     }
     this.register = this.register.bind(this);
     this.login = this.login.bind(this);
   }
+
+  
+
   login() {
     firebase.auth().signInWithEmailAndPassword(this.state.username, this.state.password).then((res) => {
       firebase.database().ref('users/' + res.user.uid).set({
         username: this.state.username,
       })
     })
+    
     alert("You are now login");
   }
   register() {
     // alert(this.state.username)
     firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(this.state.username, this.state.password).then((res) => {
-      firebase.database().ref('users/' + res.user.uid).set({
+      firebase.database().ref('users/' + res.user.uid ).set({
+        major: this.state.major,
+        info: this.state.info,
         username: this.state.username,
       })
     })
-    alert("thank you for sign in");
+    // this.setState({username: ' ', password: ''});
+    alert("Thank you for your information");
 
   }
+
   render() {
     return (
       <SafeAreaView>
         <View style={styles.loginView}>
-          <Text style={styles.loginHeader}>Login</Text>
+          <Text style={styles.loginHeader}>InfoCollector</Text>
           <Text style={styles.usernamePrompt}>Email</Text>
-          <TextInput style={styles.input} value={this.state.username} onChangeText={(text)=>this.setState({username: text})}></TextInput>
+          <TextInput style={styles.input} placeholder='you@domain.com' value={this.state.username} onChangeText={(text)=>this.setState({username: text})}></TextInput>
           <Text style={styles.usernamePrompt}>Password</Text>
-          <TextInput secureTextEntry={true} style={styles.input} value={this.state.password} onChangeText={(text)=>this.setState({password: text})}></TextInput>
-          <Button style={styles.blueBtn} title='Login' backgroundColor='red' onPress={this.login}></Button>
+          <TextInput secureTextEntry={true} autoCorrect={false} placeholder='******' style={styles.input} value={this.state.password} onChangeText={(text)=>this.setState({password: text})}></TextInput>
+          {/* <Text style={styles.usernamePrompt}>Major</Text>
+          <TextInput style={styles.input} placeholder='your current major'  value={this.state.major} onChangeText={(text)=>this.setState({major: text})}></TextInput> */}
+          {/* <Button style={styles.blueBtn} title='Login' backgroundColor='red' onPress={this.login}></Button> */}
+          
+          <Text style={styles.usernamePrompt}>Major</Text>
+          <TextInput 
+            style={styles.input}
+            value={this.state.major}
+            onChangeText={(text)=>this.setState({major: text})}
+            placeholder='Your major'></TextInput>
+          
+
+          <Text style={styles.usernamePrompt}>About yourself</Text>
+          <TextInput 
+            style={styles.input}
+            value={this.state.info}
+            onChangeText={(text)=>this.setState({info: text})}
+            placeholder='Tell something about yourself'></TextInput>
+          
           <Button style={styles.greenBtn} title='Sign Up' backgroundColor='#0096FF' onPress={this.register}></Button>
+          
+          <WeatherWidget
+            api={"9198bd7fa0fe721d6f54ce9861e973d6"}
+            lat={"33.753746"}
+            lng={"-84.386330"}
+          />
         </View>
       </SafeAreaView>
+
+      
+
     );
   }
 }
@@ -81,6 +119,7 @@ const styles = StyleSheet.create({
   },
   loginHeader: {
     fontSize: 50,
+    color: 'purple',
     fontWeight: 'bold',
     paddingBottom: 15,
   },
@@ -94,5 +133,14 @@ const styles = StyleSheet.create({
     height: 35,
     marginBottom: 20,
     backgroundColor: '#E9E9E9',
-  }
+  },
+  
+ 
+  txtInput: {
+    borderColor: 'blue',
+    borderRadius: 5,
+    height: 35,
+    marginBottom: 20,
+    backgroundColor: '#E9E9E9',
+  },
 });
